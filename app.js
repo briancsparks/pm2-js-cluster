d929f9caff3a1dac4d11a9cd4a13e7da6e264fe7
Brian C Sparks <brian.c.sparks@hp.com>
2017-08-31 06:57:42 +0000

    Refactoring

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
+
+/**
+ *  This is a PM2 module. It simply intercepts logging, and sends it to syslog.
+ */
+
 var pmx                       = require('pmx');
 var pm2                       = require('pm2');
 var SysLogger                 = require('ain2');
 
-
-//var logger    = new SysLogger({tag: 'JSCLUSTER',  facility: 'local7', hostname: process.env.MARIO_UTIL_IP, port: 10598, transport: 'UDP'});
+// The 'ain2' logging object
 var logger    = new SysLogger({tag: 'JSCLUSTER',  facility: 'local7'});
 
 
-var conf      = pmx.initModule({
-  // Override PID to be monitored
-  //pid              : pmx.resolvePidPaths(['/var/run/redis.pid']),
-}, function(err, conf) {
+var conf      = pmx.initModule({}, function(err, conf) {
   // Now the module is initialized
 
-
-  //logger.log("pm2-js-cluster", err, conf);
-
+  // Hook into the PM2 'bus'
   pm2.launchBus(function(err, bus) {
     bus.on('*', function(event, data){
       if (event == 'process:event') {
-        logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s (%s)',
+        logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
                     data.process.name,
                     data.process.pm_id,
                     data.process.restart_time,
-                    data.event,
-                    JSON.stringify(data));
+                    data.event);
       }
     });
 
     bus.on('log:err', function(data) {
-      logger.error('app=%s id=%s line=%s (%s)', data.process.name, data.process.pm_id, data.data, JSON.stringify(data));
+      logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
     });
 
     bus.on('log:out', function(data) {
-      logger.log('app=%s id=%s line=%s (%s)', data.process.name, data.process.pm_id, data.data, JSON.stringify(data));
+      logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
     });
   });
 
